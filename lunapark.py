efd37cdf4b89e432b4826581c36a1259e51f027d
Filip <muscofilip0@gmail.com>
2025-10-09 19:01:47 +0200

    Update lunapark.py

--- lunapark.py
+++ lunapark.py
@@ -2,7 +2,9 @@ godine = int(input("Unesite svoje godine: "))
 visina = int(input("Unesite svoju visinu: "))
 
 if godine < 16:
-    if visina <= 160 or visina > 190:
+    if visina > 160 or visina <= 190:
         print("smijete se voziti")
+    else:
+        print("ne smijete se voziti ")
 else:
-    print("ne smijete se voziti ")
+    print("ne smijete se voziti")
